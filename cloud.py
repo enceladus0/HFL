534de1b57d4f7871ef5114edaecb8203b88bdcbb
enceladus0 <46294409+enceladus0@users.noreply.github.com>
2023-07-10 11:18:40 +0900

    Update cloud.py

--- cloud.py
+++ cloud.py
@@ -1,16 +1,37 @@
 import flwr as fl
+import torch
+import torch.nn as nn
 
-class Cloud:
-    def configure_server(self):
-        """Returns a FedAvg strategy for the server"""
+def get_model():
+    model = nn.Sequential(
+        nn.Conv2d(3, 6, 5),
+        nn.ReLU(),
+        nn.MaxPool2d(2, 2),
+        nn.Conv2d(6, 16, 5),
+        nn.ReLU(),
+        nn.MaxPool2d(2, 2),
+        nn.Flatten(),
+        nn.Linear(16 * 5 * 5, 120),
+        nn.ReLU(),
+        nn.Linear(120, 84),
+        nn.ReLU(),
+        nn.Linear(84, 10)
+    )
+    return model
+
+class Cloud(fl.server.Server):
+    def __init__(self):
+        self.model = get_model()
+
+    def configure(self, min_clients, max_clients):
         return fl.server.strategy.FedAvg(
-            fraction_fit=0.5,  # 50% of clients used for training in each round
-            min_fit_clients=2,  # minimum number of clients to be used for training
-            min_available_clients=2,  # minimum number of clients that need to be connected to the server
+            fraction_fit=1.0,
+            fraction_eval=1.0,
+            min_fit_clients=min_clients,
+            min_eval_clients=max_clients
         )
 
 if __name__ == "__main__":
     cloud = Cloud()
-    strategy = cloud.configure_server()
-    fl.server.start_server("0.0.0.0:5000", strategy=strategy, num_rounds=3)
-
+    strategy = cloud.configure(2, 2)
+    fl.server.start_server("0.0.0.0:5000", config=strategy)
